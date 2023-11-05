fb32db1214bd40e487da0b9dba429ac59d935c7b
Sabrina <Smg21022@gmail.com>
2023-11-04 18:34:09 -0700

    got base navbar working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 import NavigationBar from './NavigationBar';
 import HomePage from './HomePage';
@@ -9,19 +15,76 @@ import BlogPostForm from './BlogPostForm';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <NavigationBar />
 
-        <Switch>
+        <Routes>
           <Route exact path="/" component={HomePage} />
           <Route path="/recipes" component={RecipesPage} />
           <Route path="/about" component={AboutMePage} />
           <Route path="/write-blog" component={BlogPostForm} />
-        </Switch>
+        </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+//
+
+//code from overstack
+// import { render } from "react-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+// // import your route components too
+
+// render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />}>
+//         <Route index element={<Home />} />
+//         <Route path="teams" element={<Teams />}>
+//           <Route path=":teamId" element={<Team />} />
+//           <Route path="new" element={<NewTeamForm />} />
+//           <Route index element={<LeagueStandings />} />
+//         </Route>
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+//original code
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+// import NavigationBar from './NavigationBar';
+// import HomePage from './HomePage';
+// import RecipesPage from './RecipesPage';
+// import AboutMePage from './AboutMePage';
+// import BlogPostForm from './BlogPostForm';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <NavigationBar />
+
+//         <Switch>
+//           <Route exact path="/" component={HomePage} />
+//           <Route path="/recipes" component={RecipesPage} />
+//           <Route path="/about" component={AboutMePage} />
+//           <Route path="/write-blog" component={BlogPostForm} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
