d9fca55399853cf0faeb67bca9e18bed27c376d6
Sabrina <Smg21022@gmail.com>
2023-11-04 17:56:16 -0700

    base_navbar_created

---
+++ NavigationBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <div className="navbar">
+      <button>Subscribe</button>
+      <button>Home</button>
+      <button>Recipes</button>
+      <button>About Me</button>
+    </div>
+  );
+}
+
+export default NavigationBar;
