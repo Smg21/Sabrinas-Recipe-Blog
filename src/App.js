f3a6178f1e2722cc5bd870e1ed81ca1e6d10595b
Sabrina <Smg21022@gmail.com>
2023-11-04 18:16:23 -0700

    fixed app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavigationBar from './NavigationBar';
+import HomePage from './HomePage';
+import RecipesPage from './RecipesPage';
+import AboutMePage from './AboutMePage';
+import BlogPostForm from './BlogPostForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/recipes" component={RecipesPage} />
+          <Route path="/about" component={AboutMePage} />
+          <Route path="/write-blog" component={BlogPostForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
