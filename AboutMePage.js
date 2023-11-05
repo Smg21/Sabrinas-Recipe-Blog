52ac62544c677ac17c3f4f5da529ddd9927db00f
Sabrina <Smg21022@gmail.com>
2023-11-04 17:58:43 -0700

    created_aboutme

---
+++ AboutMePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutMePage() {
+  return (
+    <div className="about-me">
+      <p>About Sabrina and the food blog.</p>
+    </div>
+  );
+}
+
+export default AboutMePage;
---
+++ RecipesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RecipesPage() {
+  return (
+    <div className="recipes">
+      <button>Sort by Alphabetical Order</button>
+      <button>Sort by Date Posted</button>
+      {/* List all recipe blog posts here */}
+    </div>
+  );
+}
+
+export default RecipesPage;
