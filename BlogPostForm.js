2f03bac361f836b5d3f987826c6fec740a248825
Sabrina <Smg21022@gmail.com>
2023-11-04 18:01:30 -0700

    added blogpost and reactrouter

---
+++ BlogPostForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BlogPostForm() {
+  return (
+    <div className="blog-post-form">
+      {/* Create the form to write a blog post and upload a recipe */}
+    </div>
+  );
+}
+
+export default BlogPostForm;
