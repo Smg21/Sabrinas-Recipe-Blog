1f7922181005930a328a554579f13472ac854c98
Sabrina <Smg21022@gmail.com>
2023-11-05 17:22:08 -0700

    got base for home page working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NavigationBar from './NavigationBar';
 import HomePage from './HomePage';
@@ -15,76 +9,18 @@ import BlogPostForm from './BlogPostForm';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <NavigationBar />
-
         <Routes>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/recipes" component={RecipesPage} />
-          <Route path="/about" component={AboutMePage} />
-          <Route path="/write-blog" component={BlogPostForm} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/about" element={<AboutMePage />} />
+          <Route path="/write-blog" element={<BlogPostForm />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
 export default App;
-
-//
-
-//code from overstack
-// import { render } from "react-dom";
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from "react-router-dom";
-// // import your route components too
-
-// render(
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<App />}>
-//         <Route index element={<Home />} />
-//         <Route path="teams" element={<Teams />}>
-//           <Route path=":teamId" element={<Team />} />
-//           <Route path="new" element={<NewTeamForm />} />
-//           <Route index element={<LeagueStandings />} />
-//         </Route>
-//       </Route>
-//     </Routes>
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
-
-//original code
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
-// import NavigationBar from './NavigationBar';
-// import HomePage from './HomePage';
-// import RecipesPage from './RecipesPage';
-// import AboutMePage from './AboutMePage';
-// import BlogPostForm from './BlogPostForm';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <NavigationBar />
-
-//         <Switch>
-//           <Route exact path="/" component={HomePage} />
-//           <Route path="/recipes" component={RecipesPage} />
-//           <Route path="/about" component={AboutMePage} />
-//           <Route path="/write-blog" component={BlogPostForm} />
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import './Home.css';
 
 function HomePage() {
   return (
     <div className="homepage">
-      <img src="food_logo.png" alt="Food Logo" />
-      <h1>Sabrina's Favorite Recipes</h1>
-      <a href="link_to_recent_recipe">
-        <img src="recent_recipe_image.png" alt="Recent Recipe" />
-      </a>
+      <header className="header">
+        <img src="food_logo.png" alt="Food Logo" />
+        <h1>Sabrina's Favorite Recipes</h1>
+      </header>
+      <div className="content">
+        <section className="recent-recipe">
+          <h2>Recent Recipe</h2>
+          <a href="link_to_recent_recipe">
+            <img src="recent_recipe_image.png" alt="Recent Recipe" />
+          </a>
+        </section>
+        <section className="featured-recipes">
+          <h2>Featured Recipes</h2>
+          {/* Add featured recipe components here */}
+        </section>
+      </div>
     </div>
   );
 }
