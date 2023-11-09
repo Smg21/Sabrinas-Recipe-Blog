78bfc3e77fbaf7243ecb519a9a5803ec40b5a0f1
Sabrina <Smg21022@gmail.com>
2023-11-09 10:33:59 -0700

    fake recipes created to use for time being

---
+++ src/FakeRecipe.js
@@ -0,0 +1,22 @@
+// fakeRecipes.js
+
+const fakeRecipes = [
+    {
+      id: 1,
+      title: "Spaghetti Carbonara",
+      description: "A classic Italian pasta dish with eggs, cheese, and pancetta.",
+      image: "spaghetti_carbonara.jpg",
+      link: "/recipe/spaghetti-carbonara",
+    },
+    {
+      id: 2,
+      title: "Chicken Alfredo",
+      description: "Creamy chicken and fettuccine pasta with Parmesan cheese.",
+      image: "chicken_alfredo.jpg",
+      link: "/recipe/chicken-alfredo",
+    },
+    
+  ];
+  
+  export default fakeRecipes;
+  
