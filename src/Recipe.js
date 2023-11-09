5935861c5b17d7b67f421039ec54efe087ad3431
Sabrina <Smg21022@gmail.com>
2023-11-09 10:34:40 -0700

    created recipe.js

---
+++ src/Recipe.js
@@ -0,0 +1,16 @@
+// Recipe.js
+
+import React from 'react';
+
+function Recipe({ recipe }) {
+  return (
+    <div className="recipe">
+      <img src={recipe.image} alt={recipe.title} />
+      <h3>{recipe.title}</h3>
+      <p>{recipe.description}</p>
+      <a href={recipe.link}>View Recipe</a>
+    </div>
+  );
+}
+
+export default Recipe;
